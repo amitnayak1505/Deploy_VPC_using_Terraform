3461446d319d3a3ea3cc304f7d7aabd06c7f98ca
amit <amit.nayak093@gmail.com>
2022-07-20 23:04:03 +0530

    Add ec2 instance

---
+++ ec_2.tf
@@ -0,0 +1,163 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+locals {
+    public_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
+    private_cidr = ["10.0.2.0/24", "10.0.3.0/24"]
+}
+
+resource "aws_subnet" "public" {
+ count = length(local.public_cidr)
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.public_cidr[count.index]
+
+  tags = {
+    Name = "public${count.index}"
+  }
+}
+
+resource "aws_subnet" "private" {
+ count = length(local.private_cidr)
+
+  vpc_id     = aws_vpc.main.id
+  cidr_block = local.private_cidr[count.index]
+
+  tags = {
+    Name = "private${count.index}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_eip" "nat" {
+ count = 2
+
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+ count = 2
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "public" {
+ vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table" "private" {
+ count = 2
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = {
+    Name = "private${count.index}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+ count = 2
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_route_table_association" "private" {
+ count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform-Ec2"
+  }
+}
+
+resource "aws_security_group" "securitygroup" {
+  name        = "securitygroup"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Inbound rules from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+
+  }
+  ingress {
+    description = "Inbound rules from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+
+  }
+
+  ingress {
+    description = "Inbound rules from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "securitygroup"
+  }
+}
