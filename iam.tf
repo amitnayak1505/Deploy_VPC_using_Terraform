41acd08db703fefbc617934150271b47ffcb2667
amitnayak1505 <amit.nayak093@gmail.com>
2022-10-18 21:51:38 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -2,10 +2,8 @@ module "policy-s3-full-access" {
   source  = "mineiros-io/iam-policy/aws"
   version = "~> 0.5.0"
 
-  # name of the policy
   name = "S3FullAccess"
 
-  # construct the policy document granting access from the following statements
   policy_statements = [
     {
       sid = "S3FullAccess"
